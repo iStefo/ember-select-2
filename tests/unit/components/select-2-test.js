86cb769ae690917a8f21b3503e2db447b651f0a9
Stefan Fochler <istefo@trillian.local>
2014-06-10 17:19:43 +0200

    Add tests for nested content

--- tests/unit/components/select-2-test.js
+++ tests/unit/components/select-2-test.js
@@ -29,6 +29,34 @@ var additionalContent = [
     text: "Barbecue"
   }
 ];
+var ingredients = [
+  {
+    text: "Vegetables",
+    children: [
+      {
+        id: "tom",
+        text: "Tomatoes"
+      }, {
+        id: "pin",
+        text: "Pineapples"
+      }
+    ]
+  }, {
+    text: "Meat",
+    children: [
+      {
+        id: "sal",
+        text: "Salami"
+      }, {
+        id: "ham",
+        text: "Ham"
+      }, {
+        id: "bac",
+        text: "Bacon"
+      }
+    ]
+  }
+];
 
 var App, component;
 moduleForComponent('select-2', 'Select2Component', {
@@ -311,4 +339,143 @@ test("it is disabled when its selection contains values not in the content array
   content.pushObjects(additionalContent);
 
   ok(!$('.select2-container').hasClass('select2-container-disabled'), "is enabled");
+});
+
+
+/*
+ * Nested Content
+ */
+test("(nested content) - it sets value to selected object in single selection mode", function() {
+  expect(4);
+
+  this.append();
+
+  component.set('content', ingredients);
+
+  // open options by clicking on the element
+  click('.select2-choice');
+  // then select a nested option
+  click('.select2-results li:nth-child(2) ul li:nth-child(3)', 'body');
+
+  andThen(function() {
+    strictEqual(component.get('value'), ingredients[1].children[2], "selects correct item");
+    equal($('.select2-chosen').text(), ingredients[1].children[2].text, "has correct text");
+
+    // select another option just to make sure
+    click('.select2-choice');
+    click('.select2-results li:nth-child(1) ul li:nth-child(1)', 'body');
+
+    andThen(function() {
+      strictEqual(component.get('value'), ingredients[0].children[0], "selects correct item");
+      equal($('.select2-chosen').text(), ingredients[0].children[0].text, "has correct text");
+    });
+  });
+});
+
+
+test("(nested content) - it sets value to selected object's optionValuePath in single selection mode", function() {
+  expect(4);
+
+  this.append();
+
+  component.set('content', ingredients);
+  component.set('optionValuePath', 'id');
+
+  // open options by clicking on the element
+  click('.select2-choice');
+  // then select a nested option
+  click('.select2-results li:nth-child(2) ul li:nth-child(3)', 'body');
+
+  andThen(function() {
+    strictEqual(component.get('value'), ingredients[1].children[2].id, "selects correct item");
+    equal($('.select2-chosen').text(), ingredients[1].children[2].text, "has correct text");
+
+    // select another option just to make sure
+    click('.select2-choice');
+    click('.select2-results li:nth-child(1) ul li:nth-child(1)', 'body');
+
+    andThen(function() {
+      strictEqual(component.get('value'), ingredients[0].children[0].id, "selects correct item");
+      equal($('.select2-chosen').text(), ingredients[0].children[0].text, "has correct text");
+    });
+  });
+});
+
+
+test("(nested content) - it sets value to array of selected objects in multiple selection mode", function() {
+  expect(7);
+
+  component.set('multiple', true);
+
+  this.append();
+
+  component.set('content', ingredients);
+
+  equal($('.select2-choices').text().trim(), '', "has empty selection text on start");
+
+  // select an option after opening the dropdown
+  click('.select2-choices');
+  click('.select2-results li:nth-child(2) ul li:nth-child(3)', 'body');
+
+  andThen(function() {
+    deepEqual(component.get('value'), [ingredients[1].children[2]], "has correct value");
+    equal($('.select2-choices').text().replace(/ /g, ''), ingredients[1].children[2].text, "displays correct text");
+
+    // select another item
+    click('.select2-choices');
+    click('.select2-results li:nth-child(1) ul li:nth-child(1)', 'body');
+
+    andThen(function() {
+      deepEqual(component.get('value'), [ingredients[1].children[2], ingredients[0].children[0]], "has correct value");
+      equal($('.select2-choices').text().replace(/ /g, ''), ingredients[1].children[2].text + ingredients[0].children[0].text, "displays correct text");
+
+      // remove the first item again
+      click('.select2-search-choice:nth-child(1) .select2-search-choice-close');
+
+      andThen(function() {
+        deepEqual(component.get('value'), [ingredients[0].children[0]], "has correct value");
+        equal($('.select2-choices').text().replace(/ /g, ''), ingredients[0].children[0].text, "displays correct text");
+      });
+    });
+  });
+});
+
+
+test("(nested content) - it sets value to array of selected objects' optionValuePaths in multiple selection mode", function() {
+  expect(7);
+
+  component.set('multiple', true);
+
+  this.append();
+
+  component.set('content', ingredients);
+  component.set('optionValuePath', 'id');
+
+  equal($('.select2-choices').text().trim(), '', "has empty selection text on start");
+
+  // select an option after opening the dropdown
+  click('.select2-choices');
+  click('.select2-results li:nth-child(2) ul li:nth-child(3)', 'body');
+
+  andThen(function() {
+    deepEqual(component.get('value'), [ingredients[1].children[2].id], "has correct value");
+    equal($('.select2-choices').text().replace(/ /g, ''), ingredients[1].children[2].text, "displays correct text");
+
+    // select another item
+    click('.select2-choices');
+    click('.select2-results li:nth-child(1) ul li:nth-child(1)', 'body');
+
+    andThen(function() {
+      deepEqual(component.get('value'), [ingredients[1].children[2].id, ingredients[0].children[0].id], "has correct value");
+      equal($('.select2-choices').text().replace(/ /g, ''), ingredients[1].children[2].text + ingredients[0].children[0].text, "displays correct text");
+
+      // remove the first item again
+      click('.select2-search-choice:nth-child(1) .select2-search-choice-close');
+
+      andThen(function() {
+        deepEqual(component.get('value'), [ingredients[0].children[0].id], "has correct value");
+        equal($('.select2-choices').text().replace(/ /g, ''), ingredients[0].children[0].text, "displays correct text");
+      });
+    });
+  });
 });
