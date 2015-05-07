faee3202e82ce339eaf2f12338e54dd07d0f60ec
Steffen Brem <steffenbrem@gmail.com>
2015-05-07 02:39:17 +0200

    Added some additional info and cleaned up the test a bit.

--- tests/unit/components/select-2-tags-test.js
+++ tests/unit/components/select-2-tags-test.js
@@ -46,21 +46,9 @@ test("it can have ember data record array as content", function(assert) {
 
   click('.select2-search-field');
   fillIn('.select2-input', 'tag 2');
-
-  Ember.run.later(function() {
-
-  }, 2000);
+  click('.select2-results li:first-child');
 
   andThen(function() {
-    alert(find('.select2-results li:first-child').text());
     assert.equal(component.get('value.lastObject.name'), 'tag 2');
-  });
-
-  Ember.run.later(function() {
-
-  }, 1000);
-
-  andThen(function() {
-    assert.equal(1, 1);
   });
 });
